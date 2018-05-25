9c75a2c67e0c8762a4bb270e468bd062d4d6317e
Noah Scharrenberg <nscharrenberg@hotmail.com>
2018-05-25 10:46:59 +0200

    port added, Skeleton adjusted.

--- openbadges/src/components/Skeleton.js
+++ openbadges/src/components/Skeleton.js
@@ -5,202 +5,216 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuItem from '@material-ui/core/MenuItem';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import styling from '../assets/css/index.css';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import StarIcon from '@material-ui/icons/Star';
+import SendIcon from '@material-ui/icons/Send';
+import MailIcon from '@material-ui/icons/Mail';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ReportIcon from '@material-ui/icons/Report';
+import Routes from './Routes';
+import Button from '@material-ui/core/Button';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    Redirect
 } from 'react-router-dom';
 
-import Routes from './Routes';
-import Nav from './Nav';
-
-const drawerWidth = 200;
+const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-	flexGrow: 1,
-	height: '100vh',
-  },
-  appFrame: {
-	zIndex: 1,
-	overflow: 'hidden',
-	position: 'relative',
-	display: 'flex',
-	width: '100%',
-	height: '100vh',
-  },
-  appBar: {
-	position: 'absolute',
-	transition: theme.transitions.create(['margin', 'width'], {
-	  easing: theme.transitions.easing.sharp,
-	  duration: theme.transitions.duration.leavingScreen,
-	}),
-  },
-  appBarShift: {
-	width: `calc(100% - ${drawerWidth}px)`,
-	transition: theme.transitions.create(['margin', 'width'], {
-	  easing: theme.transitions.easing.easeOut,
-	  duration: theme.transitions.duration.enteringScreen,
-	}),
-  },
-  'appBarShift-left': {
-	marginLeft: drawerWidth,
-  },
-  'appBarShift-right': {
-	marginRight: drawerWidth,
-  },
-  menuButton: {
-	marginLeft: 12,
-	marginRight: 20,
-  },
-  hide: {
-	display: 'none',
-  },
-  drawerPaper: {
-	position: 'relative',
-	width: drawerWidth,
-  },
-  drawerHeader: {
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'flex-end',
-	padding: '0 8px',
-	...theme.mixins.toolbar,
-  },
-  content: {
-	flexGrow: 1,
-	backgroundColor: theme.palette.background.default,
-	padding: theme.spacing.unit * 3,
-	transition: theme.transitions.create('margin', {
-	  easing: theme.transitions.easing.sharp,
-	  duration: theme.transitions.duration.leavingScreen,
-	}),
-  },
-  'content-left': {
-	marginLeft: -drawerWidth,
-  },
-  'content-right': {
-	marginRight: -drawerWidth,
-  },
-  contentShift: {
-	transition: theme.transitions.create('margin', {
-	  easing: theme.transitions.easing.easeOut,
-	  duration: theme.transitions.duration.enteringScreen,
-	}),
-  },
-  'contentShift-left': {
-	marginLeft: 0,
-  },
-  'contentShift-right': {
-	marginRight: 0,
-  },
-  'flex': {
-	flex: 1,
-  },
+    root: {
+        flexGrow: 1,
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawerPaper: {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing.unit * 7,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing.unit * 9,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+		...theme.mixins.toolbar,
+	},
+	content: {
+		flexGrow: 1,
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing.unit * 3,
+	},
+	flex: {
+    	flex: 1,
+	}
 });
 
-class PersistentDrawer extends React.Component {
-  state = {
-	open: false,
-	anchor: 'left',
-  };
+class MiniDrawer extends React.Component {
+	state = {
+		open: true,
+	};
 
-  handleDrawerOpen = () => {
-	this.setState({ open: true });
-  };
+	handleDrawerOpen = () => {
+		this.setState({ open: true });
+	};
 
-  handleDrawerClose = () => {
-	this.setState({ open: false });
-  };
+	handleDrawerClose = () => {
+		this.setState({ open: false });
+	};
 
-  handleChangeAnchor = event => {
-	this.setState({
-	  anchor: event.target.value,
-	});
-  };
+	render() {
+		const { classes, theme } = this.props;
 
-  render() {
-	const { classes, theme } = this.props;
-	const { anchor, open } = this.state;
-
-	const drawer = (
-	  <Drawer onClick={this.handleDrawerClose}
-		variant="persistent"
-		anchor={anchor}
-		open={open}
-		classes={{
-		  paper: classes.drawerPaper,
-		}}
-	  >
-		<div className={classes.drawerHeader} >
-		  <IconButton onClick={this.handleDrawerClose}>
-			<ChevronLeftIcon />
-		  </IconButton>
-		</div>
-		<Divider />
-		<Nav />
-	  </Drawer>
-	);
-
-	return (
-	  <div className={classes.root}>
-		<div className={classes.appFrame}>
-		  <AppBar
-			className={classNames(classes.appBar, {
-			  [classes.appBarShift]: open,
-			  [classes[`appBarShift-${anchor}`]]: open,
-			})}
-		  >
-			<Toolbar disableGutters={!open}>
-			  <IconButton
-				color="inherit"
-				aria-label="open drawer"
-				onClick={this.handleDrawerOpen}
-				className={classNames(classes.menuButton, open && classes.hide)}
-			  >
-				<MenuIcon />
-			  </IconButton>
-			  <Typography variant="title" color="inherit" className={classes.flex}>
-				Blockchain & Open Badges
-			  </Typography>
-			  <Link to="/profile"><Button color="inherit">Login</Button></Link>
-			</Toolbar>
-		  </AppBar>
-		  {drawer}
-		  <main
-			className={classNames(classes.content, classes[`content-${anchor}`], {
-			  [classes.contentShift]: open,
-			  [classes[`contentShift-${anchor}`]]: open,
-			})}
-		  >
-			<div className={classes.drawerHeader} />
-			<div onClick={this.handleDrawerClose}>
-				<Routes />
+		return (
+			<div className={classes.root}>
+				<AppBar
+					position="absolute"
+					className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+				>
+					<Toolbar disableGutters={!this.state.open}>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={this.handleDrawerOpen}
+							className={classNames(classes.menuButton, this.state.open && classes.hide)}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="title" color="inherit" className={classes.flex}>
+							Open Badges
+						</Typography>
+                        <Link to="/profile"><Button color="inherit">Login</Button></Link>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="permanent"
+					classes={{
+						paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+					}}
+					open={this.state.open}
+				>
+					<div className={classes.toolbar}>
+						<IconButton onClick={this.handleDrawerClose}>
+							{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+						</IconButton>
+					</div>
+					<Divider />
+					<List>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <StarIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Starred" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <SendIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Send mail" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </ListItem>
+					</List>
+					<Divider />
+					<List>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <MailIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="All mail" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <DeleteIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Trash" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <ReportIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Spam" />
+                        </ListItem>
+					</List>
+				</Drawer>
+                    <main
+                        className={classNames(classes.content)}
+                    >
+                        <div className={classes.drawerHeader} />
+                        <div onClick={this.handleDrawerClose}>
+                            <Routes />
+                        </div>
+				</main>
 			</div>
-		  </main>
-		</div>
-	  </div>
-	);
-  }
+		);
+	}
 }
 
-PersistentDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+MiniDrawer.propTypes = {
+	classes: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(PersistentDrawer);
+export default withStyles(styles, { withTheme: true })(MiniDrawer);
