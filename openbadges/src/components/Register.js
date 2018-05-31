acba104e2f99ed6f9db48b1573294a1fce49b099
Noah Scharrenberg <nscharrenberg@hotmail.com>
2018-05-31 11:56:29 +0200

    Axios API

--- openbadges/src/components/Register.js
+++ openbadges/src/components/Register.js
@@ -11,6 +11,7 @@ import TextField from '@material-ui/core/TextField';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Lock from '@material-ui/icons/Lock';
 import Email from '@material-ui/icons/Email';
+import axios from 'axios';
 
 
 const styles = theme => ({
@@ -29,6 +30,23 @@ const styles = theme => ({
 
 
 class Register extends React.Component {
+    state = {
+        staff: []
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        axios.post('http://192.168.27.142:3000/api/org.acme.empty.BadgeUser', {data})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
         const { classes, theme } = this.props;
 
@@ -42,63 +60,66 @@ class Register extends React.Component {
                                     <h1>Register</h1>
                                 </Grid>
                             </Grid>
-                            <div className={classes.margin}>
+                            <form onSubmit={this.handleSubmit}>
+                                <input name="entityType" id="entityType" value="SomeType" />
+                                <div className={classes.margin}>
+                                    <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
+                                        <Grid item>
+                                            <AccountCircle />
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="firstName" name="firstName" label="First name" />
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                                <div className={classes.margin}>
+                                    <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
+                                        <Grid item>
+                                            <AccountCircle />
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="lastName" name="lastName" label="Last name" />
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                                <div className={classes.margin}>
+                                    <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
+                                        <Grid item>
+                                            <AccountCircle />
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="username" label="Username" />
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                                <div className={classes.margin}>
+                                    <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
+                                        <Grid item>
+                                            <Email />
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="emails" label="Email" />
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                                <div className={classes.margin}>
+                                    <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
+                                        <Grid item>
+                                            <Lock />
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="password" type="password" label="Password" />
+                                        </Grid>
+                                    </Grid>
+                                </div>
                                 <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField id="firstname" label="First name" />
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <div className={classes.margin}>
-                                <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField id="lastname" label="Last name" />
+                                    <Grid item xs={6}>
+                                        <Button type="submit"  style={{width: '100%'}} size={"large"} color={"primary"} variant="raised" component="button" className={classes.button}>
+                                            Register
+                                        </Button>
                                     </Grid>
                                 </Grid>
-                            </div>
-                            <div className={classes.margin}>
-                                <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                    <Grid item>
-                                        <AccountCircle />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField id="username" label="Username" />
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <div className={classes.margin}>
-                                <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                    <Grid item>
-                                        <Email />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField id="email" label="Email" />
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <div className={classes.margin}>
-                                <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                    <Grid item>
-                                        <Lock />
-                                    </Grid>
-                                    <Grid item>
-                                        <TextField id="password" type="password" label="Password" />
-                                    </Grid>
-                                </Grid>
-                            </div>
-                            <Grid container spacing={24} alignItems={"flex-end"} justify={"center"}>
-                                <Grid item xs={6}>
-                                    <Button style={{width: '100%'}} size={"large"} color={"primary"} variant="raised" href="#raised-buttons" className={classes.button}>
-                                        Register
-                                    </Button>
-                                </Grid>
-                            </Grid>
+                            </form>
                         </Paper>
                     </Grid>
                 </Grid>
